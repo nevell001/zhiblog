cfb028f4f4a265a8ca506ef631a1e8a3c5dc26b0
nevell <nevell@foxmail.com>
2026-08-12 15:57:46 +0800

    fix(frontend): makeMap 未命中键返回 false 以符合 boolean 类型声明

--- zhi-ui/src/utils/index.ts
+++ zhi-ui/src/utils/index.ts
@@ -342,7 +342,7 @@ export function makeMap(str: string, expectsLowerCase?: boolean): (val: string) 
   for (let i = 0; i < list.length; i++) {
     map[list[i]] = true
   }
-  return expectsLowerCase ? val => map[val.toLowerCase()] : val => map[val]
+  return expectsLowerCase ? val => !!map[val.toLowerCase()] : val => !!map[val]
 }
 
 export const exportDefault = 'export default '
